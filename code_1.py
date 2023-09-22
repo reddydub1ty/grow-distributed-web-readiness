97103b2f5927352ad1d41b78e9d91207d99ce1d1
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-09-22 02:10:20 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+}
+    return data;
+    data.forEach(item => {
+    const data = generateRandomData();
+function generateRandomData() {
+}
+        console.log("Random Number: " + item);
+    }
+
+main();
+    });
+function main() {
+
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
